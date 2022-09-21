974dc947aed932699b6e354b1a6421ba76346ea6
gdeedler <grantdeedler@gmail.com>
2022-09-20 22:16:24 -0400

    Create docker container

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const database = require('./database')
+const database = require('./database');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.port || 9000;
+const PORT = process.env.PORT || 9000;
+const DATABASEIP = process.env.LOCALIP || 'localhost';
 
 app.use(morgan('dev'));
 
-(async function () {
-  await mongoose.connect('mongodb://localhost:27017/products');
+const run = () => {main().catch(err => {
+  console.log(err);
+  run();
+})}
+
+run();
+
+async function main() {
+  console.log('Connecting to database')
+  await mongoose.connect(`mongodb://${DATABASEIP}:27017/products`);
   console.log('Connected to database');
 
-  app.get('/products', async(req, res) => {
+  app.get('/products', async (req, res) => {
     const page = req.query.page;
     const count = req.query.count;
     const response = await database.getPage(page, count);
     res.status(200);
     res.send(response);
-  })
+  });
 
-  app.get('/:id/styles', async(req, res) => {
+  app.get('/:id/styles', async (req, res) => {
     const response = await database.getStyles(req.params.id);
     res.status(200);
     res.send(response);
-  })
+  });
 
   app.get('/:id/related', async (req, res) => {
     const response = await database.getRelated(req.params.id);
     res.status(200);
     res.send(response);
-  })
+  });
 
   app.listen(PORT, () => {
     console.log(`Products database listening on port ${PORT}`);
   });
-})().catch((err) => console.log(err));
+}
